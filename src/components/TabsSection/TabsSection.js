c87d21c0f9a96ce3f1c6e3424e50b63cd95b7502
Abdalrahman-Mohammad <abd0004@hotmail.com>
2025-05-05 11:31:17 +0300

    Add X logo

--- src/components/TabsSection/TabsSection.js
+++ src/components/TabsSection/TabsSection.js
@@ -1,6 +1,10 @@
 import "./TabsSection.css";
 function TabsSection() {
-  return <div className="tabs-container">tabs section</div>;
+  return (
+    <div className="tabs-container">
+      <img src="/X_logo.png" alt="x logo" width={50}/>
+    </div>
+  );
 }
 
 export default TabsSection;
